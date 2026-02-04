f643b2c6f4b589212a29c89a7afe06a4c31d8bae
gabhu-dev <gabhu@hotmail.es>
2026-02-04 18:09:31 -0500

    [add] proyects pages and styles

---
+++ src/@constants/proyects.ts
@@ -0,0 +1,39 @@
+import ProyectImage from "../assets/images/proyect.png";
+// Importa otros iconos si es necesario, por ejemplo de @lucide/astro
+// import { Code, Globe } from "@lucide/astro";
+
+export interface Tag {
+  icon: any; // O string si prefieres nombres de iconos
+  label: string;
+}
+
+export interface Project {
+  slug: string;
+  title: string;
+  link: string;
+  image: ImageMetadata;
+  description: string;
+  tags: Tag[];
+  responsiveImage: {
+    link: ImageMetadata;
+    responsive: boolean;
+  };
+}
+
+export const projects: Project[] = [
+  {
+    slug: "landing-dennky",
+    title: "LANDING DENNKY",
+    link: "https://dennky.com",
+    image: ProyectImage,
+    description: "Laboratorio de fluidos dieléctricos y lubricantes. Somos una empresa que realiza servicios de muestreo, análisis de fluidos dieléctricos y de aceites lubricantes.",
+    tags: [
+      { icon: "Code", label: "Astro" },
+      { icon: "Layout", label: "Tailwind" },
+    ],
+    responsiveImage: {
+      link: ProyectImage,
+      responsive: true,
+    },
+  },
+];
