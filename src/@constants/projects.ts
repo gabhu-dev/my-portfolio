9e8faf94892cc14ba447cce50cb022ae7726ac48
gabhu-dev <gabhu@hotmail.es>
2026-02-07 12:03:12 -0500

    [add] linformation for landing

--- src/@constants/projects.ts
+++ src/@constants/projects.ts
@@ -7,7 +7,7 @@ import ProjectTickerResponsive from "../assets/images/proyect-ticker-responsive.
 import ProjectDennky from "../assets/images/proyect-dennky.png";
 import ProjectDennkyDuval from "../assets/images/proyect-dennky-2.png";
 
-import { FigmaIcon, PencilIcon, CodeIcon } from "@lucide/astro";
+import { FigmaIcon, PencilIcon, CodeIcon, BrushIcon, PaletteIcon } from "@lucide/astro";
 
 export interface Tag {
   icon: any; // O string si prefieres nombres de iconos
@@ -42,8 +42,8 @@ export const PROJECTS_DATA: Project[] = [
       { icon: CodeIcon, label: "Ant Design Vue" },
       { icon: CodeIcon, label: "Socket.io" },
       { icon: CodeIcon, label: "Axios" },
-      { icon: PencilIcon, label: "Sass" },
-      { icon: PencilIcon, label: "Responsive Design" },
+      { icon: BrushIcon, label: "Sass" },
+      { icon: BrushIcon, label: "Responsive Design" },
       { icon: FigmaIcon, label: "Figma" },
     ],
     responsiveImage: {
@@ -57,10 +57,15 @@ export const PROJECTS_DATA: Project[] = [
     titlePage: "LANDING DENNKY",
     link: "https://dennky.com",
     image: ProjectLanding,
-    description: "Laboratorio de fluidos dieléctricos y lubricantes. Somos una empresa que realiza servicios de muestreo, análisis de fluidos dieléctricos y de aceites lubricantes.",
+    description: "La landing fue creada como la página oficial de un laboratorio, con el objetivo de <span class='font-semibold'>presentar de manera clara y atractiva los servicios que ofrecen</span> 🔬.<br><br>En este proyecto me encargué de:<br><br>• 🎨 Diseñar la interfaz, cuidando la identidad visual y la experiencia del usuario.<br>• 🛠️ Construir la landing, asegurando que fuera responsiva y fácil de navegar.<br>• 📲 Integrar un botón de contacto por WhatsApp, lo que permitió que Dennky sea más accesible y que los clientes lleguen directamente a través de este canal.<br><br>El resultado fue una landing page que no solo comunica los servicios del laboratorio de forma efectiva, sino que también <span class='font-semibold'>mejoró la captación de clientes y la facilidad de contacto</span> 🚀.",
     tags: [
+      { icon: CodeIcon, label: "Nuxt" },
+      { icon: CodeIcon, label: "Vite" },
+      { icon: CodeIcon, label: "TS" },
+      { icon: BrushIcon, label: "Sass" },
+      { icon: PaletteIcon, label: "Tailwind" },
+      { icon: BrushIcon, label: "Responsive Design" },
       { icon: FigmaIcon, label: "Figma" },
-      { icon: PencilIcon, label: "Tailwind" },
     ],
     responsiveImage: {
       link: ProjectLandingResponsive,
